33490d8b12752cf4c31544baff28e19652956c11
killershin <sinyc0619@naver.com>
2024-10-18 14:22:25 +0900

    PetClinic commit
    PetClinic commit

---
+++ bak/src/main/java/com/ktds/e2edemo/St02E2edemoApplication.java
@@ -0,0 +1,13 @@
+package com.ktds.e2edemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class St02E2edemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(St02E2edemoApplication.class, args);
+	}
+
+}
